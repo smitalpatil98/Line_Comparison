7e62320c8e54b19150bbac601cb60d40dbb1aa76
smitalpatil98 <patilsmital.1101998@gmail.com>
2024-06-19 06:34:53 +0530

    Update Line_Comparison.java

--- src/main/java/com/linecomparison/Line_Comparison.java
+++ src/main/java/com/linecomparison/Line_Comparison.java
@@ -2,5 +2,8 @@ package com.linecomparison;
 
 public class Line_Comparison
 {
-    
+    public static void main(String[]args)
+    {
+ System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+   }
 }
