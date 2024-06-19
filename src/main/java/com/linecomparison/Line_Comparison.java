8c17fa9b94ee30533ddb0fbaad4d0dd5d38fc913
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-19 06:48:22 +0530

    UC_2_Equality_of_two_lines

--- src/main/java/com/linecomparison/Line_Comparison.java
+++ src/main/java/com/linecomparison/Line_Comparison.java
@@ -2,15 +2,24 @@ package com.linecomparison;
 
 public class Line_Comparison
 {
-    public void cartesiansystem(double x1,double x2,double y1,double y2)
+    public Double isEquallines(double x1,double x2,double y1,double y2)
     {
-        double length_of_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-        System.out.println("Length of line is :"+length_of_line);
+        double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        return length_line;
     }
 
     public static void main(String[] args)
     {
         Line_Comparison lc = new Line_Comparison();
-        lc.cartesiansystem(1, 5, 4, 9);
+        double length_line1 = lc.isEquallines(1, 5, 4, 9);
+        double length_line2 = lc.isEquallines(2,6,4,8);
+        if(length_line1 == length_line2){
+            System.out.println("Lines are Equal");
+        }
+        else
+        {
+            System.out.println("Lines are Not Equal");
+        }
     }
-}
+    }
+
